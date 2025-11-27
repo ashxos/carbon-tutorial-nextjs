2f96522e462828ff6ab770948b25473b6ad42393
ashxos <ashxos@gmail.com>
2025-11-27 13:56:17 +0530

    feat(tutorial): complete step 3

--- src/app/repos/page.js
+++ src/app/repos/page.js
@@ -1,7 +1,59 @@
 'use client';
 
 import RepoTable from './RepoTable';
-import { Column, Grid } from '@carbon/react';
+import { Octokit } from '@octokit/core';
+import { Link, Grid, Column } from '@carbon/react';
+import React, { useEffect, useState } from 'react';
+
+const octokitClient = new Octokit({});
+const LinkList = ({ url, homepageUrl }) => (
+  <ul style={{ display: 'flex' }}>
+    <li>
+      <Link href={url}>GitHub</Link>
+    </li>
+    {homepageUrl && (
+      <li>
+        <span>&nbsp;|&nbsp;</span>
+        <Link href={homepageUrl}>Homepage</Link>
+      </li>
+    )}
+  </ul>
+);
+
+const getRowItems = (rows) =>
+  rows.map((row) => ({
+    ...row,
+    key: row.id,
+    stars: row.stargazers_count,
+    issueCount: row.open_issues_count,
+    createdAt: new Date(row.created_at).toLocaleDateString(),
+    updatedAt: new Date(row.updated_at).toLocaleDateString(),
+    links: <LinkList url={row.html_url} homepageUrl={row.homepage} />,
+  }));
+
+function RepoPage() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+  const [rows, setRows] = useState([]);
+  useEffect(() => {
+    async function getCarbonRepos() {
+      const res = await octokitClient.request('GET /orgs/{org}/repos', {
+        org: 'carbon-design-system',
+        per_page: 75,
+        sort: 'updated',
+        direction: 'desc',
+      });
+
+      if (res.status === 200) {
+        console.log(res.data);
+      } else {
+        console.log('Error obtaining repository data');
+      }
+    }
+
+    getCarbonRepos();
+  }, []);
+}
 
 const headers = [
   {
@@ -60,14 +112,14 @@ const rows = [
   },
 ];
 
-function RepoPage() {
-  return (
-    <Grid className="repo-page">
-      <Column lg={16} md={8} sm={4} className="repo-page__r1">
-        <RepoTable headers={headers} rows={rows} />
-      </Column>
-    </Grid>
-  );
-}
+// function RepoPage() {
+//   return (
+//     <Grid className="repo-page">
+//       <Column lg={16} md={8} sm={4} className="repo-page__r1">
+//         <RepoTable headers={headers} rows={rows} />
+//       </Column>
+//     </Grid>
+//   );
+// }
 
 export default RepoPage;
